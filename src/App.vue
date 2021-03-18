25b11d064409477f110d213affd63b73a9b26a0b
ArthurVBottcher <arthur.vinicius@kunden.com.br>
2021-03-18 16:06:13 -0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app> <Home /> </v-app>
 </template>
 
+<script lang="ts">
+import Vue from "vue";
+import Home from "./views/Home.vue";
+export default Vue.extend({
+  name: "App",
+
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+});
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url("https://fonts.googleapis.com/css?family=Share+Tech+Mono");
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+* {
+  padding: 0;
+  margin: 0;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  font-family: "Share Tech Mono", sans-serif;
+  overflow-y: auto !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,99 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <!-- <div>
+    <v-system-bar window dark height="30px">
+      <span>StopWatch</span>
+      <v-icon color="white">mdi-minus</v-icon>
+      <v-icon>mdi-checkbox-blank-outline</v-icon>
+      <v-icon>mdi-close</v-icon>
+    </v-system-bar> -->
+  <v-card color="#ddd" tile class="wrapper" width="400px" height="150px">
+    <v-card-title
+      class="justify-center mt-2"
+      label
+      :text-color="color"
+      style="font-size: 50px"
+    >
+      {{ watch }}
+    </v-card-title>
+    <v-card-actions class="justify-space-around pa-4">
+      <v-btn
+        color="success"
+        class="mx-2"
+        width="100px"
+        outlined
+        @click="timeStart"
+      >
+        Start</v-btn
+      >
+
+      <v-btn
+        color="error"
+        class="mx-2"
+        width="100px"
+        outlined
+        @click="timePaused"
+      >
+        Pause
+      </v-btn>
+
+      <v-btn
+        color="warning"
+        class="mx-2"
+        width="100px"
+        outlined
+        @click="timeReset"
+        >Reset</v-btn
+      >
+    </v-card-actions>
+  </v-card>
+  <!-- </div> -->
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data: () => {
+    return {
+      watch: "00:00:00:00",
+      millisecound: 0,
+      timer: 0,
+      color: "",
+    };
+  },
+  methods: {
+    timeStart() {
+      this.color = "info";
+      clearInterval(this.timer);
+      this.timer = window.setInterval(() => {
+        this.millisecound += 10;
+        let dateTimer = new Date(this.millisecound);
+
+        this.watch =
+          ("0" + dateTimer.getUTCHours()).slice(-2) +
+          ":" +
+          ("0" + dateTimer.getUTCMinutes()).slice(-2) +
+          ":" +
+          ("0" + dateTimer.getUTCSeconds()).slice(-2) +
+          ":" +
+          ("0" + dateTimer.getUTCMilliseconds()).slice(-3, -1);
+      }, 10);
+    },
+
+    timePaused() {
+      this.color = "error";
+      clearInterval(this.timer);
+    },
+
+    timeReset(): void {
+      this.timer;
+      this.millisecound = 0;
+      this.watch = "00:00:00:00";
+      this.color = "black";
+    },
   },
 });
 </script>
+<style lang="scss"></style>
